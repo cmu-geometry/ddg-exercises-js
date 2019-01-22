0581bb515aa91bf575f4f8243b22a82ea38572da
Mark Gillespie <mark@iMac.wv.cc.cmu.edu>
2019-01-22 15:38:53 -0500

    Added simplicial complex assignment

---
+++ projects/simplicial-complex-operators/simplicial-complex-operators.js
@@ -0,0 +1,140 @@
+"use strict";
+
+class SimplicialComplexOperators {
+
+        /** This class implements various operators (e.g. boundary, star, link) on a mesh.
+         * @constructor module:Projects.SimplicialComplexOperators
+         * @param {module:Core.Mesh} mesh The input mesh this class acts on.
+         * @property {module:Core.Mesh} mesh The input mesh this class acts on.
+         * @property {module:LinearAlgebra.SparseMatrix} A0 The vertex-edge adjacency matrix of <code>mesh</code>.
+         * @property {module:LinearAlgebra.SparseMatrix} A1 The edge-face adjacency matrix of <code>mesh</code>.
+         */
+        constructor(mesh) {
+                this.mesh = mesh;
+                this.assignElementIndices(this.mesh);
+
+                this.A0 = this.buildVertexEdgeAdjacencyMatrix(this.mesh);
+                this.A1 = this.buildEdgeFaceAdjacencyMatrix(this.mesh);
+        }
+
+        /** Assigns indices to the input mesh's vertices, edges, and faces
+         * @method module:Projects.SimplicialComplexOperators#assignElementIndices
+         * @param {module:Core.Mesh} mesh The input mesh which we index.
+         */
+        assignElementIndices(mesh) {
+                // TODO
+        }
+
+        /** Returns the vertex-edge adjacency matrix of the given mesh.
+         * @method module:Projects.SimplicialComplexOperators#buildVertexEdgeAdjacencyMatrix
+         * @param {module:Core.Mesh} mesh The mesh whose adjacency matrix we compute.
+         * @returns {module:LinearAlgebra.DenseMatrix} The vertex-edge adjacency matrix of the given mesh.
+         */
+        buildVertexEdgeAdjacencyMatrix(mesh) {
+                // TODO
+        }
+
+        /** Returns the edge-face adjacency matrix.
+         * @method module:Projects.SimplicialComplexOperators#buildEdgeFaceAdjacencyMatrix
+         * @param {module:Core.Mesh} mesh The mesh whose adjacency matrix we compute.
+         * @returns {module:LinearAlgebra.DenseMatrix} The edge-face adjacency matrix of the given mesh.
+         */
+        buildEdgeFaceAdjacencyMatrix(mesh) {
+                // TODO
+        }
+
+        /** Returns a column vector representing the vertices of the
+         * given subset.
+         * @method module:Projects.SimplicialComplexOperators#buildVertexVector
+         * @param {module:Core.MeshSubset} subset A subset of our mesh.
+         * @returns {module:LinearAlgebra.DenseMatrix} A column vector with |V| entries. The ith entry is 1 if
+         *  vertex i is in the given subset and 0 otherwise
+         */
+        buildVertexVector(subset) {
+                // TODO
+        }
+
+        /** Returns a column vector representing the edges of the
+         * given subset.
+         * @method module:Projects.SimplicialComplexOperators#buildEdgeVector
+         * @param {module:Core.MeshSubset} subset A subset of our mesh.
+         * @returns {module:LinearAlgebra.DenseMatrix} A column vector with |E| entries. The ith entry is 1 if
+         *  edge i is in the given subset and 0 otherwise
+         */
+        buildEdgeVector(subset) {
+                // TODO
+        }
+
+        /** Returns a column vector representing the faces of the
+         * given subset.
+         * @method module:Projects.SimplicialComplexOperators#buildFaceVector
+         * @param {module:Core.MeshSubset} subset A subset of our mesh.
+         * @returns {module:LinearAlgebra.DenseMatrix} A column vector with |F| entries. The ith entry is 1 if
+         *  face i is in the given subset and 0 otherwise
+         */
+        buildFaceVector(subset) {
+                // TODO
+        }
+
+        /** Returns the star of a subset.
+         * @method module:Projects.SimplicialComplexOperators#star
+         * @param {module:Core.MeshSubset} subset A subset of our mesh.
+         * @returns {module:Core.MeshSubset} The star of the given subset.
+         */
+        star(subset) {
+                // TODO
+
+                return subset; // placeholder
+        }
+
+        /** Returns the closure of a subset.
+         * @method module:Projects.SimplicialComplexOperators#closure
+         * @param {module:Core.MeshSubset} subset A subset of our mesh.
+         * @returns {module:Core.MeshSubset} The closure of the given subset.
+         */
+        closure(subset) {
+                // TODO
+
+                return subset; // placeholder
+        }
+
+        /** Returns the link of a subset.
+         * @method module:Projects.SimplicialComplexOperators#link
+         * @param {module:Core.MeshSubset} subset A subset of our mesh.
+         * @returns {module:Core.MeshSubset} The link of the given subset.
+         */
+        link(subset) {
+                // TODO
+
+                return subset; // placeholder
+        }
+
+        /** Returns true if the given subset is a subcomplex and false otherwise.
+         * @method module:Projects.SimplicialComplexOperators#isSubcomplex
+         * @param {module:Core.MeshSubset} subset A subset of our mesh.
+         * @returns {boolean} True if the given subset is a subcomplex and false otherwise.
+         */
+        isComplex(subset) {
+                // TODO
+        }
+
+        /** Returns the degree if the given subset is a pure subcomplex and -1 otherwise.
+         * @method module:Projects.SimplicialComplexOperators#pureDegree
+         * @param {module:Core.MeshSubset} subset A subset of our mesh.
+         * @returns {number} The degree of the given subset if it is a pure subcomplex and -1 otherwise.
+         */
+        isPureComplex(subset) {
+                // TODO
+        }
+
+        /** Returns the boundary of a subset.
+         * @method module:Projects.SimplicialComplexOperators#boundary
+         * @param {module:Core.MeshSubset} subset A subset of our mesh. We assume <code>subset</code> is a pure subcomplex.
+         * @returns {module:Core.MeshSubset} The boundary of the given pure subcomplex.
+         */
+        boundary(subset) {
+                // TODO
+
+                return subset; // placeholder
+        }
+}
